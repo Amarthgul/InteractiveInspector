e63b075d977dba597c6cff7ed852eba499a792ca
Amarth Gul <amarthgul@gmail.com>
2024-02-07 11:14:25 -0500

    Separated the class as an interface

---
+++ Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ObjectBehavior
+{
+    public void GazeOperation(float LoC); 
+}
--- Assets/Scripts/StarGazer.cs
+++ Assets/Scripts/StarGazer.cs
@@ -9,17 +9,6 @@ public class StarGazer : MonoBehaviour
     /// ==================== Serialized variables ===================== 
     [SerializeField] Camera meinCamera; // Das war ein befehl!
 
-    [Tooltip("Possibility of gaze is represented by color")]
-    [SerializeField] private bool enableColor;
-
-    [ShowIf(ActionOnConditionFail.DisableOnly, ConditionOperator.And, nameof(enableColor))]
-    [Tooltip("Color when the object is believed to be gazed upon")]
-    [SerializeField] private Color focusColor = new Color(1, 0, 0);
-
-    [ShowIf(ActionOnConditionFail.DisableOnly, ConditionOperator.And, nameof(enableColor))]
-    [Tooltip("Color when the object is believed to be inrelevant")]
-    [SerializeField] private Color neglectColor = new Color (0, 0, 1);
-
     [Tooltip("Objects that can interact with the star gazer")]
     [SerializeField] List<GameObject> gazableObjects = new List<GameObject>();
 
@@ -88,51 +77,12 @@ public class StarGazer : MonoBehaviour
 
             // Calculate the color and opacity 
             gazeConfidence[gazableObjects[i]] = LoC;
-            Color sheen = InterpolateFocusColor(LoC);
 
-            /// =============================================================== 
-            if (enableColor) // Assign the color and opacity if desired 
-            {
-                RecursionChildColor(gazableObjects[i], sheen, LoC);
-            }
+            gazableObjects[i].GetComponent<ObjectBehavior>().GazeOperation(LoC);
         }
     }
     
-    /// <summary>
-    /// Recursively enter each object and change its sheen color/opacity. 
-    /// </summary>
-    /// <param name="thisObject">Object to check</param>
-    /// <param name="sheenColor">Goal sheen color</param>
-    /// <param name="LoC">Level of Confidence as opacity</param>
-    private void RecursionChildColor(GameObject thisObject, Color sheenColor, float LoC)
-    {
-        // Keep doing recusion if this child also has child objects
-        if (thisObject.transform.childCount > 0)
-        {
-            for (int i = 0; i < thisObject.transform.childCount; i++)
-                RecursionChildColor(thisObject.transform.GetChild(i).gameObject, sheenColor, LoC);
-        }
-        // Change the color and effect opacity if this is the parent object 
-        else if(thisObject.GetComponent<Renderer>() != null) // If this is the parent object 
-        {
-            thisObject.GetComponent<Renderer>().material.SetColor("_Sheen", sheenColor);
-            thisObject.GetComponent<Renderer>().material.SetFloat("_Opacity", LoC);
-        }
-    }
-
-    /// <summary>
-    /// Interpolate between 2 colors. 
-    /// </summary>
-    /// <param name="LoC">Level of confidence</param>
-    /// <returns>Color interpolated between high and low representations</returns>
-    private Color InterpolateFocusColor(float LoC)
-    {
-        Color color = new Color();
-
-        color = LoC * focusColor + (1 - LoC) * neglectColor; 
-
-        return color; 
-    }
+    
 
     /// <summary>
     /// Calculate the camera diagonal field of view and update it. 
